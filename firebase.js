2359b1723728998c0f4c1a9014b53594379b2299
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-02-18 22:06:43 +0900

    202302182200

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+import firebase from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqDJaDWIZ6bD5SWPjc3nWTBH7CqThvgxg",
+  authDomain: "clone-7852b.firebaseapp.com",
+  projectId: "clone-7852b",
+  storageBucket: "clone-7852b.appspot.com",
+  messagingSenderId: "915954214969",
+  appId: "1:915954214969:web:439d9973ee48d159c0c5e8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -5,13 +5,23 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
-const Header = () => {
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../../slices/basketSlice";
+
+function Header() {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
+
   return (
     <header>
       {/* topheader */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -19,7 +29,6 @@ const Header = () => {
             className="cursor-pointer"
           />
         </div>
-
         {/* search */}
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
           <input
@@ -30,17 +39,20 @@ const Header = () => {
         </div>
         {/* right side */}
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
-            <p>Hello satoshi</p>
+          <div onClick={!session ? signIn : signOut} className="link">
+            <p> {session ? `hello ${session.user.name}` : "sign in"}</p>
             <p className="font-extrabold md:text-sm">Account & List</p>
           </div>
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="link relative flex items-center">
+          <div
+            onClick={() => router.push("/checkout")}
+            className="link relative flex items-center"
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black">
-              1
+              {items.length}
             </span>
 
             <ShoppingCartIcon className="h-10" />
@@ -70,6 +82,6 @@ const Header = () => {
       </div>
     </header>
   );
-};
+}
 
 export default Header;
